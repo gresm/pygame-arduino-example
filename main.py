6bbc69b9e67c80449a9e82043a13a50791260222
gresm <78505251+gresm@users.noreply.github.com>
2022-12-31 10:54:58 +0100

    Re-implement drawing & food spawning

--- main.py
+++ main.py
@@ -86,25 +86,40 @@ def main():
 
 def game(display: pg.Surface, clock: pg.time.Clock):
     size = display.get_size()
+    dot_size = (10, 10)
     board_size = ((size[0] // 10) - 10, (size[1] // 10) - 10)
-    food = {(26, 25)}
-    snake = deque([(25, 25)])
-    snake_segments: set[tuple[int, int]] = {(25, 25)}
-    snake_x = 25
-    snake_y = 25
+    board_rect = pg.Rect(0, 0, board_size[0] * dot_size[0], board_size[1] * dot_size[1])
+    board_rect.center = (size[0] // 2, size[1] // 2)
+    snake_x = board_size[0] // 2
+    snake_y = board_size[1] // 2
+    food = {(snake_x + 1, snake_y)}
+    snake = deque([(snake_x, snake_y)])
+    snake_segments: set[tuple[int, int]] = {(snake_x, snake_y)}
     snake_x_change = 1
     snake_y_change = 0
     food_eat_cycle = 0
     required_food = 2
-    frames_per_snake_step = 30
+    frames_per_snake_step = 60
     frames_to_snake_step = 0
+    snake_step_increment = 2
+    snake_min_fp_step = 30
     snake_alive = True
+    player_won = False
     dead_show_red = False
     font = pg.font.SysFont("", 30)
     death_message = "You have died. PRESS OK to play again."
+    win_message = "You have won. PRESS OK to play again."
     death_surface = font.render(death_message, False, (127, 127, 127))
+    win_surface = font.render(win_message, False, (0, 127, 0))
     death_rect = death_surface.get_rect()
+    win_rect = win_surface.get_rect()
     death_rect.center = (size[0] // 2, size[1] - 25)
+    win_rect.center = (size[0] // 2, size[1] - 25)
+
+    def draw_dot(x, y, color):
+        pg.draw.rect(
+            display, color, ((x * dot_size[0] + board_rect.left, y * dot_size[1] + board_rect.top), dot_size)
+        )
 
     def check_pos(x: int, y: int, do_food: bool):
         if not (0 <= x < board_size[0]) or not (0 <= y < board_size[1]):
@@ -140,32 +155,43 @@ def game(display: pg.Surface, clock: pg.time.Clock):
         tested = set()
         possible = set()
 
-        def possible_add(xy):
-            if xy not in tested:
-                possible.add(xy)
+        def possible_add(x0, y0):
+            if check_pos(x0, y0, True) and (x0, y0) not in tested:
+                possible.add((x0, y0))
 
         def populate_possible():
             if x - 1 >= 0:
-                possible_add((x - 1, y))
+                possible_add(x - 1, y)
             if y - 1 >= 0:
-                possible_add((x, y - 1))
+                possible_add(x, y - 1)
             if x + 1 >= board_size[0]:
-                possible_add((x + 1, y))
+                possible_add(x + 1, y)
             if y + 1 >= board_size[1]:
-                possible_add((x, y + 1))
+                possible_add(x, y + 1)
 
-        # TODO: finish flood fill
-        return False
         populate_possible()
         while len(possible):
             goto = possible.copy()
             possible.clear()
-            for pos in possible:
-                pass
+            for pos in goto:
+                x = pos[0]
+                y = pos[1]
+                if check_pos(x, y, True):
+                    return True
+                tested.add(pos)
+                populate_possible()
+        return False
 
     def spawn_food():
+        nonlocal snake_alive, player_won
+        success = True
         for _ in range(required_food - len(food)):
-            spawn_thing()
+            success = spawn_thing()
+            if not success:
+                break
+        if not success:
+            snake_alive = False
+            player_won = True
 
     def move_snake():
         nonlocal food_eat_cycle, required_food, frames_per_snake_step, snake_x, snake_y
@@ -179,9 +205,9 @@ def game(display: pg.Surface, clock: pg.time.Clock):
             if food_eat_cycle >= 3:
                 food_eat_cycle = 0
                 required_food += 1
-                frames_per_snake_step -= 2
-                if frames_per_snake_step < 4:
-                    frames_per_snake_step = 4
+                frames_per_snake_step -= snake_step_increment
+                if frames_per_snake_step < snake_min_fp_step:
+                    frames_per_snake_step = snake_min_fp_step
         else:
             snake_segments.remove(snake.popleft())
 
@@ -228,18 +254,24 @@ def game(display: pg.Surface, clock: pg.time.Clock):
                 dead_show_red = not dead_show_red
 
         display.fill((0, 0, 0))
-        pg.draw.rect(display, (255, 255, 255), (50, 50, 500, 500))
+        pg.draw.rect(display, (255, 255, 255), board_rect)
 
         for thing in food:
-            pg.draw.rect(display, (255, 0, 0), ((thing[0] + 5) * 10, (thing[1] + 5) * 10, 10, 10))
+            draw_dot(thing[0], thing[1], (255, 0, 0))
+            # pg.draw.rect(display, (255, 0, 0), ((thing[0] + 5) * 10, (thing[1] + 5) * 10, 10, 10))
 
         for segment in snake:
-            pg.draw.rect(display, (0, 255, 0), ((segment[0] + 5) * 10, (segment[1] + 5) * 10, 10, 10))
+            draw_dot(segment[0], segment[1], (0, 255, 0))
+            # pg.draw.rect(display, (0, 255, 0), ((segment[0] + 5) * 10, (segment[1] + 5) * 10, 10, 10))
 
         if not snake_alive:
-            display.blit(death_surface, death_rect)
+            if player_won:
+                display.blit(win_surface, win_rect)
+            else:
+                display.blit(death_surface, death_rect)
             if dead_show_red:
-                pg.draw.rect(display, (255, 0, 0), ((snake_x + 5) * 10, (snake_y + 5) * 10, 10, 10))
+                draw_dot(snake_x, snake_y, (255, 0, 0))
+                # pg.draw.rect(display, (255, 0, 0), ((snake_x + 5) * 10, (snake_y + 5) * 10, 10, 10))
 
         pg.display.flip()
         clock.tick(60)
